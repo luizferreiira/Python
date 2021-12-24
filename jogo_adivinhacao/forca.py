9fe22a5b3b2238ad2beff9cd7f623ee3365db7a2
luizferreiira <luizferreiraa34@gmail.com>
2021-12-24 02:22:52 -0300

    jogo_adivinhacao/

--- jogo_adivinhacao/forca.py
+++ jogo_adivinhacao/forca.py
@@ -3,4 +3,25 @@ def jogar():
     print("Bem-vindo ao jogo de forca")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = str(input("Qual letra? \n"))
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute,index))
+
+            index+=1
+
+        print("Jogando...")
+
     print("Fim do jogo")
+
+if(__name__=="__main__"):
+    jogar()
